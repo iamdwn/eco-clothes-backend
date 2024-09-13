b27433f08d3595b2807abc14ca9ec24f39f9b2f2
handgun <DungPHASE172122@fpt.edu.vn>
2024-09-14 00:50:23 +0700

    UPDATE - Update source

---
+++ src/ApiGateways/OcelotGateway/Program.cs
@@ -0,0 +1,37 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json");
+
+var services = builder.Services;
+
+// Add services to the container.
+
+services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+// Config Ocelot
+services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+await app.UseOcelot();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
