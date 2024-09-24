5aae67009f0af4f8bdde825456e5546e5a5ebb01
iamdwn <truongdinhdongduong@gmail.com>
2024-09-25 02:27:35 +0700

    add generic-repository and unit-of-work

---
+++ src/BuildingBlocks/DataAccess/Base/IUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.Models;
+
+namespace DataAccess.Base
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Cart> CartRepository { get; }
+        IGenericRepository<Category> CategoryRepository { get; }
+        IGenericRepository<Favorite> FavoriteRepository { get; }
+        IGenericRepository<Feedback> FeedbackRepository { get; }
+        IGenericRepository<Order> OrderRepository { get; }
+        IGenericRepository<Orderitem> OrderitemRepository { get; }
+        IGenericRepository<Payment> PaymentRepository { get; }
+        IGenericRepository<Paymentsubscription> PaymentsubscriptionRepository { get; }
+        IGenericRepository<Product> ProductRepository { get; }
+        IGenericRepository<Productcategory> ProductcategoryRepository { get; }
+        IGenericRepository<Size> SizeRepository { get; }
+        IGenericRepository<Sizeproduct> SizeproductRepository { get; }
+        IGenericRepository<Subscription> SubscriptionRepository { get; }
+        IGenericRepository<User> UserRepository { get; }
+
+        void Save();
+    }
+}
