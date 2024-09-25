ebf4812e03615f0600381d15a3f910e4d6a09390
iamdwn <truongdinhdongduong@gmail.com>
2024-09-26 01:17:38 +0700

    feat: add CRUD product

--- src/Services/Products/Products.Api/Controllers/ProductsController.cs
+++ src/Services/Products/Products.Api/Controllers/ProductsController.cs
@@ -14,35 +14,30 @@ namespace Products.Api.Controllers
             _productService = productService;
         }
 
-        // GET: api/products
         [HttpGet]
         public async Task<IEnumerable<Product>> GetProducts()
         {
             return await _productService.GetAllProductsAsync();
         }
 
-        // GET: api/products/{id}
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(string id)
         {
             return await _productService.GetProductByIdAsync(id);
         }
 
-        // POST: api/products
         [HttpPost]
         public async Task<ActionResult<Product>> CreateProduct(Product product)
         {
             return await _productService.CreateProductAsync(product);
         }
 
-        // PUT: api/products/{id}
         [HttpPut]
         public async Task UpdateProduct(Product product)
         {
             await _productService.UpdateProductAsync(product);
         }
 
-        // DELETE: api/products/{id}
         [HttpDelete("{id}")]
         public async Task DeleteProduct(string id)
         {
