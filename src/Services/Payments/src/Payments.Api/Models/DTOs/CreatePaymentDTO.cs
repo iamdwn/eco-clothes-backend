c37b89aa37326d2c2222d202c983039bf7203b42
handgun <DungPHASE172122@fpt.edu.vn>
2024-10-05 08:24:44 +0700

    Add Users Service

---
+++ src/Services/Payments/src/Payments.Api/Models/DTOs/CreatePaymentDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Payments.Api.Models.DTOs
+{
+    public class CreatePaymentDTO
+    {
+        public string UserId { get; set; }
+        public double Amount { get; set; }
+    }
+}
---
+++ src/Services/Payments/src/Payments.Api/Services/Interfaces/IPaymentService.cs
@@ -0,0 +1,11 @@
+﻿using Payments.Api.Models.DTOs;
+
+namespace Payments.Api.Services.Interfaces
+{
+    public interface IPaymentService
+    {
+        Task<string> CreatePayment(CreatePaymentDTO model);
+        Task VNPayResponse();
+        Task MoMoResponse();
+    }
+}
