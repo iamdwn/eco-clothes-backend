cee663803e501362c7a5cf4795dfb513bcf4092f
handgun <DungPHASE172122@fpt.edu.vn>
2024-10-10 15:45:39 +0700

    Add ISoftDelete to User entity

--- src/BuildingBlocks/DataAccess/Migrations/20241010084156_AddISoftDeleteToUser.Designer.cs
+++ src/BuildingBlocks/DataAccess/Migrations/20241010084156_AddISoftDeleteToUser.Designer.cs
@@ -4,6 +4,7 @@ using DataAccess.Persistences;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(EcoClothesContext))]
-    partial class EcoClothesContextModelSnapshot : ModelSnapshot
+    [Migration("20241010084156_AddISoftDeleteToUser")]
+    partial class AddISoftDeleteToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
