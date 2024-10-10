fb687c242440ebc54a45f1b36b98670f1d26c9a8
handgun <DungPHASE172122@fpt.edu.vn>
2024-10-10 15:35:35 +0700

    Update UserService

--- src/Services/Users/src/Users.Api/Services/Interfaces/IUserService.cs
+++ src/Services/Users/src/Users.Api/Services/Interfaces/IUserService.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess.Models;
+using Microsoft.EntityFrameworkCore.Update.Internal;
 using Users.Api.Dtos.Request;
 
 namespace Users.Api.Services.Interfaces
@@ -6,5 +7,7 @@ namespace Users.Api.Services.Interfaces
     public interface IUserService
     {
         Task<User> CreateUser(CreateUserDTO model);
+        Task<User> UpdateUser(UpdateUsetDTO model);
+        Task<bool> DeleteUser(string id);
     }
 }
