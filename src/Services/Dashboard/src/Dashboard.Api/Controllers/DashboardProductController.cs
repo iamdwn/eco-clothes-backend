c575e8ba75bf6303a8d1f9ca3576b0d1f05c2e1f
iamdwn <truongdinhdongduong@gmail.com>
2024-10-11 19:21:18 +0700

    feat: add product dashboard

---
+++ src/Services/Dashboard/src/Dashboard.Api/Controllers/DashboardProductController.cs
@@ -0,0 +1,42 @@
+﻿using Dashboard.Api.Services;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dashboard.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DashboardProductController : Controller
+    {
+        private readonly IProductAnalytics _productAnalytics;
+
+        public DashboardProductController(IProductAnalytics productAnalytics)
+        {
+            _productAnalytics = productAnalytics;
+        }
+
+        [HttpGet("total-products")]
+        public async Task<IEnumerable<Product>> GetTotalProducts()
+        {
+            return await _productAnalytics.GetTotalProductsAsync();
+        }
+
+        [HttpGet("in-stock-products")]
+        public async Task<IEnumerable<Product>> GetInStockProducts()
+        {
+            return await _productAnalytics.GetInStockProductsAsync();
+        }
+
+        [HttpGet("count-products")]
+        public async Task<int> CountProducts()
+        {
+            return await _productAnalytics.CountProductsAsync();
+        }
+
+        [HttpGet("count-in-stock-products")]
+        public async Task<int> CountInStockProducts()
+        {
+            return await _productAnalytics.CountInStockProductsAsync();
+        }
+    }
+}
---
+++ src/Services/Dashboard/src/Dashboard.Api/Services/Impl/ProductAnalyticsImpl.cs
@@ -0,0 +1,39 @@
+﻿using DataAccess.Base;
+using DataAccess.Models;
+
+namespace Dashboard.Api.Services.Impl
+{
+    public class ProductAnalyticsImpl : IProductAnalytics
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductAnalyticsImpl(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<int> CountInStockProductsAsync()
+        {
+            return _unitOfWork.ProductRepository.Get(
+                filter: u => u.Amount > 0
+                ).Count();
+        }
+
+        public async Task<int> CountProductsAsync()
+        {
+            return _unitOfWork.ProductRepository.Get().Count();
+        }
+
+        public async Task<IEnumerable<Product>> GetInStockProductsAsync()
+        {
+            return _unitOfWork.ProductRepository.Get(
+                filter: u => u.Amount > 0
+                ).ToList();
+        }
+
+        public async Task<IEnumerable<Product>> GetTotalProductsAsync()
+        {
+            return _unitOfWork.ProductRepository.Get().ToList();
+        }
+    }
+}
