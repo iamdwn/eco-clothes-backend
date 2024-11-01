25f9200d9fa1c6054379b6dd822f6bd9e9b9c88f
iamdwn <truongdinhdongduong@gmail.com>
2024-11-01 08:27:55 +0700

    feat: add category controller and catch exception

--- src/Services/Carts/src/Carts.Api/Controllers/CartController.cs
+++ src/Services/Carts/src/Carts.Api/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using Carts.Api.Dtos;
 using Carts.Api.Services;
 using DataAccess.Models;
+using DataAccess.Models.Response;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Carts.Api.Controllers
@@ -22,45 +23,95 @@ namespace Carts.Api.Controllers
         }
 
         [HttpGet("by-id/{id}")]
-        public async Task<Cart> GetCartById(Guid id)
+        public async Task<ResponseObject> GetCartById(Guid id)
         {
-            return await _cartService.GetCartByIdAsync(id);
+            try
+            {
+                return ResponseObject.Success<Cart>(await _cartService.GetCartByIdAsync(id));
+            }
+            catch (Exception ex)
+            {
+                return ResponseObject.Failure(ex.Message);
+            }
         }
 
         [HttpGet("by-user-id/{userId}")]
-        public async Task<IEnumerable<Cart>> GetCartByUserId(Guid userId)
+        public async Task<ResponseObject> GetCartByUserId(Guid userId)
         {
-            return await _cartService.GetCartByUserIdAsync(userId);
+            try
+            {
+                return ResponseObject.Success<IEnumerable<Cart>>(await _cartService.GetCartByUserIdAsync(userId));
+            }
+            catch (Exception ex)
+            {
+                return ResponseObject.Failure(ex.Message);
+            }
         }
 
         [HttpGet("total-price-of-cart/{userId}")]
-        public async Task<double> TotalPriceOfCart(Guid userId)
+        public async Task<ResponseObject> TotalPriceOfCart(Guid userId)
         {
-            return await _cartService.TotalPriceOfCartAsync(userId);
+            try
+            {
+                return ResponseObject.Success<double>(await _cartService.TotalPriceOfCartAsync(userId));
+            }
+            catch (Exception ex)
+            {
+                return ResponseObject.Failure(ex.Message);
+            }
         }
 
         [HttpGet("count-products-of-cart/{userId}")]
-        public async Task<int> CountProductsOfCart(Guid userId)
+        public async Task<ResponseObject> CountProductsOfCart(Guid userId)
         {
-            return await _cartService.CountProductsOfCartAsync(userId);
+            try
+            {
+                return ResponseObject.Success<int>(await _cartService.CountProductsOfCartAsync(userId));
+            }
+            catch (Exception ex)
+            {
+                return ResponseObject.Failure(ex.Message);
+            }
         }
 
         [HttpGet("shipping-fee-of-cart/{userId}")]
-        public async Task<double> ShippingFeeOfCart(Guid userId)
+        public async Task<ResponseObject> ShippingFeeOfCart(Guid userId)
         {
-            return await _cartService.ShippingFeeOfCartAsync(userId);
+            try
+            {
+                return ResponseObject.Success<double>(await _cartService.ShippingFeeOfCartAsync(userId));
+            }
+            catch (Exception ex)
+            {
+                return ResponseObject.Failure(ex.Message);
+            }
         }
 
         [HttpPost]
-        public async Task<ActionResult<Cart>> AddToCart(CartDto cart)
+        public async Task<ResponseObject> AddToCart(CartDto cart)
         {
-            return await _cartService.AddToCartAsync(cart);
+            try
+            {
+                return ResponseObject.Success<Cart>(await _cartService.AddToCartAsync(cart));
+            }
+            catch (Exception ex)
+            {
+                return ResponseObject.Failure(ex.Message);
+            }
         }
 
         [HttpDelete("{userId}")]
-        public async Task DeleteCart(Guid userId)
+        public async Task<ResponseObject> DeleteCart(Guid userId)
         {
-            await _cartService.DeleteCartAsync(userId);
+            try
+            {
+                await _cartService.DeleteCartAsync(userId);
+                return ResponseObject.Success();
+            }
+            catch (Exception ex)
+            {
+                return ResponseObject.Failure(ex.Message);
+            }
         }
     }
 }
